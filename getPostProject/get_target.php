72edae52a793a05750db107c0b908c6ad2e35477
Gr34v0 <gr34v0@gmail.com>
2018-02-08 14:02:06 -0500

    Added dropdown tab for the Projects section and added my About page to the project section since it was actually a project.

--- getPostProject/get_target.php
+++ getPostProject/get_target.php
@@ -1,11 +1,14 @@
 <html>
     <title>GET</title>
     <head>
-        <link rel="stylesheet" href="../resources/site-wide.css">
+        <link rel="stylesheet" href="../resources/css/site-wide.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../resources/css/bootstrap-dropdownhover.min.css">
+        <link rel="stylesheet" href="../resources/css/animate.css">
         <script src="//code.jquery.com/jquery.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="../resources/js/bootstrap-dropdownhover.min.js"></script>
     </head>
 
     <div id="nav-placeholder">
