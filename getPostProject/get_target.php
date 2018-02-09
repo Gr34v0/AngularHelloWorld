bffc3bbe7ff6f4f6b283e721b2d79fa40ef7e52a
Gr34v0 <gr34v0@gmail.com>
2018-02-09 13:59:54 -0500

    Fixed problems regarding the body tags on each page. Implemented bootstrap panels to make things generic but better than pure whitespace. Added footer to drag in to each page the same as header.

--- getPostProject/get_target.php
+++ getPostProject/get_target.php
@@ -12,21 +12,36 @@
         </script>
     </div>
 
-    <h1>GET Example</h1>
-    <h2>
-        <a href="get.html">GET Example Homepage</a>
-    </h2>
-    <h2>
-        <a href="post.html">POST Example Homepage</a>
-    </h2>
-    <hr>
     <body id="body-text">
-        <p>First Name: <?php echo $_GET['fname']; ?></p>
-        <p>Last Name: <?php echo $_GET['lname']; ?></p>
-        <p>Age: <?php echo $_GET['age']; ?></p>
-        <p>Gender: <?php echo $_GET['gender']; ?></p>
-        <p>State: <?php echo $_GET['state']; ?></p>
-        <hr>
-        <p>Notice how information is appearing in the URL header?</p>
+
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h1>GET Example</h1>
+                <h2>
+                    <a href="get.html">GET Example Homepage</a>
+                </h2>
+                <h2>
+                    <a href="post.html">POST Example Homepage</a>
+                </h2>
+            </div>
+            <div class="panel-body">
+                <p>First Name: <?php echo $_GET['fname']; ?></p>
+                <p>Last Name: <?php echo $_GET['lname']; ?></p>
+                <p>Age: <?php echo $_GET['age']; ?></p>
+                <p>Gender: <?php echo $_GET['gender']; ?></p>
+                <p>State: <?php echo $_GET['state']; ?></p>
+                <hr>
+                <p>Notice how information is appearing in the URL header?</p>
+            </div>
+        </div>
+
     </body>
+
+    <div id="footer-placeholder">
+        <script>
+            $.get("../base/footer.html", function(data){
+                $("#footer-placeholder").replaceWith(data);
+            });
+        </script>
+    </div>
 </html>
--- getPostProject/post_target.php
+++ getPostProject/post_target.php
@@ -12,21 +12,36 @@
         </script>
     </div>
 
-    <h1>POST Example</h1>
-    <h2>
-        <a href="get.html">GET Example Homepage</a>
-    </h2>
-    <h2>
-        <a href="post.html">POST Example Homepage</a>
-    </h2>
-    <hr>
     <body id="body-text">
-        <p>First Name: <?php echo $_POST['fname']; ?></p>
-        <p>Last Name: <?php echo $_POST['lname']; ?></p>
-        <p>Age: <?php echo $_POST['age']; ?></p>
-        <p>Gender: <?php echo $_POST['gender']; ?></p>
-        <p>State: <?php echo $_POST['state']; ?></p>
-        <hr>
-        <p>Notice how no information is appearing in the URL header?</p>
+
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h1>POST Example</h1>
+                <h2>
+                    <a href="get.html">GET Example Homepage</a>
+                </h2>
+                <h2>
+                    <a href="post.html">POST Example Homepage</a>
+                </h2>
+            </div>
+            <div class="panel-body">
+                <p>First Name: <?php echo $_POST['fname']; ?></p>
+                <p>Last Name: <?php echo $_POST['lname']; ?></p>
+                <p>Age: <?php echo $_POST['age']; ?></p>
+                <p>Gender: <?php echo $_POST['gender']; ?></p>
+                <p>State: <?php echo $_POST['state']; ?></p>
+                <hr>
+                <p>Notice how no information is appearing in the URL header?</p>
+            </div>
+        </div>
+
     </body>
+
+    <div id="footer-placeholder">
+        <script>
+            $.get("../base/footer.html", function(data){
+                $("#footer-placeholder").replaceWith(data);
+            });
+        </script>
+    </div>
 </html>
