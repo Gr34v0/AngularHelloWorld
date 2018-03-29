04617f7f672948ddbb72f645bce55336c56d9e70
gr34v0 <gr34v0@gmail.com>
2018-03-29 19:38:14 -0400

    Login, and favoriting frontend

--- NewsRSS/newsJS.js
+++ NewsRSS/newsJS.js
@@ -5,6 +5,7 @@ window.onload = init;
 
 function init(){
 
+    var html = "";
     var urls = new Array();
     var stringUrl = "Checked: ";
 
@@ -17,13 +18,15 @@ function init(){
 
     console.log(urls);
 
-    document.querySelector("#checked").innerHTML = "<p>" + stringUrl + "</p>";
-
-    document.querySelector("#content").innerHTML = "<b>Loading news...</b>";
     $("#content").fadeOut(250);
+
+    document.querySelector("#content").innerHTML = "";
+
+    //document.querySelector("#checked").innerHTML = "<p>" + stringUrl + "</p>";
+
     //fetch the data
     urls.forEach(function(url){
-        $.get(url).done(function(data){xmlLoaded(data);});
+        $.get(url).done(function(data){html = xmlLoaded(data);});
     });
 }
 
@@ -49,20 +52,90 @@ function xmlLoaded(obj){
         var description = newsItem.querySelector("description").firstChild.nodeValue;
         var link = newsItem.querySelector("link").firstChild.nodeValue;
         var pubDate = newsItem.querySelector("pubDate").firstChild.nodeValue;
+        var parsedDate = Date.parse(pubDate);
 
         //present the item as HTML
-        var line = '<div class="item">';
+        var line = '<div data-newsdate='+ parsedDate +' class="item">';
         line += "<h2>"+ origin + "</h2>";
         line += "<h2>" + title + "</h2>";
-        line += '<p><i>'+pubDate+'</i> - <a href="'+link+'" target="_blank">See original</a></p>';
+        line += '<p><i id="pubDate">'+pubDate+'</i> - <a href="'+link+'" target="_blank">See original</a> - <a href="#" onclick="addFav()">Add to Favorites</a></p>';
         //title and description are always the same (for some reason) so I'm only including one
         //line += "<p>"+description+"</p>";
         line += "</div>";
 
         html += line;
     }
-    document.querySelector("#content").innerHTML = html;
+
+    console.log(html);
+
+    //date sorting ... Date.parse( pubDate HERE );
+
+    document.querySelector("#content").innerHTML += html;
 
     $("#content").fadeIn(1000);
+
+}
+
+function favs(){
+
+    var items = obj.querySelectorAll("item");
+    var origin = obj.querySelector("title").firstChild.nodeValue;
+    //show the logo
+    var image = obj.querySelector("image");
+    var logoSrc = image.querySelector("url").firstChild.nodeValue;
+    var logoLink = image.querySelector("link").firstChild.nodeValue;
+    $("#logo").attr("src",logoSrc);
+
+    //parse the data
+    var html = "";
+    for (var i=0;i<items.length;i++){
+        //get the data out of the item
+        var newsItem = items[i];
+        var title = newsItem.querySelector("title").firstChild.nodeValue;
+        console.log(title);
+        var description = newsItem.querySelector("description").firstChild.nodeValue;
+        var link = newsItem.querySelector("link").firstChild.nodeValue;
+        var pubDate = newsItem.querySelector("pubDate").firstChild.nodeValue;
+        var parsedDate = Date.parse(pubDate);
+
+        //present the item as HTML
+        var line = '<div data-newsdate='+ parsedDate +' class="item">';
+        line += "<h2>"+ origin + "</h2>";
+        line += "<h2>" + title + "</h2>";
+        line += '<p><i id="pubDate">'+pubDate+'</i> - <a href="'+link+'" target="_blank">See original</a> - <a href="#" onclick="addFav()">Add to Favorites</a></p>';
+        //title and description are always the same (for some reason) so I'm only including one
+        //line += "<p>"+description+"</p>";
+        line += "</div>";
+
+        html += line;
+    }
+
+    console.log(html);
+
+    //date sorting ... Date.parse( pubDate HERE );
+
+    document.querySelector("#content").innerHTML += html;
+
+    $("#content").fadeIn(1000);
+
+}
+
+function addFav(){
+
+}
+
+
+
+function alreadyMember(){
+
+    $("#register").css('display', 'none');
+    $("#login").css('display', 'block');
+
+}
+
+function notMember(){
+
+    $("#login").css('display', 'none');
+    $("#register").css('display', 'block');
 
 }
